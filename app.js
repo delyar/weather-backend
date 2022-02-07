76bd328ed2b008ad14004ef19e6199ae49396875
Delyar Tabatabai <tabatase@oregonstate.edu>
2022-02-06 19:54:47 -0800

    secured the endpoints

--- app.js
+++ app.js
@@ -11,29 +11,37 @@ app.use(cors());
 
 app.get('/v1/weather',get_weather_v1)
 app.get('/v1/hello',get_hello)
+app.post('/v1/auth',post_auth)
 
 
 
 function get_weather_v1(request, response) {
-     response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+    if(request.headers.authorization == "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkRlbHlhciBUYWJhIiwiaWF0IjoxNTE2MjM5MDIyfQ.lDFREfBxRaXwHhRS0nMQseFcYKbCCwwEuA0Ms5HzQjA")
+       response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
+    response.json("Invalid")
 }
 
 function get_hello(req,res){
     //res.set('content-type', 'application/json')
-    res.json({"hello": "Hi I am Delyar"})
+    if(req.headers.authorization == "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkRlbHlhciBUYWJhIiwiaWF0IjoxNTE2MjM5MDIyfQ.lDFREfBxRaXwHhRS0nMQseFcYKbCCwwEuA0Ms5HzQjA")
+      res.json({"hello": "Hi I am Delyar"})
+    res.json("Invalid")
 }
 // const fs = require("fs");
-app.post('/v1/auth',post_auth)
+
 function post_auth(req,res){
-    let usernames = ['Delyar','Taba','hi']
-    let passwords = ['123','456','789']
+    let usernames = ['Delyar']
+    let passwords = ['123']
     // const obj = JSON.parse(req.body)
     let username = req.body.username
     let pwd = req.body.password
 
     if(usernames.includes(username)){
       if(passwords.includes(pwd)){
-          res.json({ "Sucess" : "Your API key is here: 53f0a8a769a368154b48b7a48ee1bb0f"})
+          res.json({
+            "access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkRlbHlhciBUYWJhIiwiaWF0IjoxNTE2MjM5MDIyfQ.lDFREfBxRaXwHhRS0nMQseFcYKbCCwwEuA0Ms5HzQjA",
+            "expires": new Date()
+          })
       }
   }
 }
